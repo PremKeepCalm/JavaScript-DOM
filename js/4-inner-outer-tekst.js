2757347a4f917c6f3ab068531f839fe02a3110a3
Przemyslaw Kaczor <przem.kacz7@gmai.com>
2019-03-03 12:16:16 +0100

    add-remove

--- js/4-inner-outer-tekst.js
+++ js/4-inner-outer-tekst.js
@@ -3,4 +3,12 @@ let header = document.querySelector('header');
 
 header.lastElementChild.textContent += '!';
 
-console.log(document.querySelector('.parSecond').outerHTML);
+// console.log(document.querySelector('.parSecond').outerHTML);
+
+let link = document.querySelector('.link.superlink');
+
+link.classList.add('nowa-klasa');
+link.classList.remove('nowa-klasa');
+
+console.log(link.classList);
+
